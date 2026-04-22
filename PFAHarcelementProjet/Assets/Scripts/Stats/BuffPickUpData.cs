65eaad5e63cc14b9a13c649a53474f77f5f26269
Maxence Lemaitre <maxencelemaitre028@gmail.com>
2026-04-22 16:18:35 +0200

    67

---
+++ PFAHarcelementProjet/Assets/Scripts/Stats/BuffPickUpData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BuffPickup", menuName = "Roguelike/Buff Pickup")]
+public class BuffPickupData : ScriptableObject
+{
+    [Header("Visuel")]
+    public string buffName    = "Nouveau buff";
+    public string description = "Description du buff";
+    public Sprite icon;
+
+    [Header("Modificateurs")]
+    public List<StatModifierData> modifiers = new List<StatModifierData>();
+}
+
+[System.Serializable]
+public class StatModifierData
+{
+    public StatType     statType;
+    public ModifierType modifierType;
+    public float        value;
+}
