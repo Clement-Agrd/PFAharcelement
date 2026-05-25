bb761766df9a95ae2db867614175efaac6da3eed
clement <clement.augoyard@gmail.com>
2026-05-25 14:50:36 +0200

    plein de truc

--- PFAHarcelementProjet/Assets/Scripts/clement/IAEnemi/EnemySpawner.cs
+++ PFAHarcelementProjet/Assets/Scripts/clement/IAEnemi/EnemySpawner.cs
@@ -7,5 +7,13 @@ public class EnemySpawner : MonoBehaviour, IRoomEnter
     public void OnRoomEnter()
     {
         Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+
+        // ✅ On notifie que des ennemis existent
+        CombatRoomEndTrigger trigger = FindObjectOfType<CombatRoomEndTrigger>();
+
+        if (trigger != null)
+        {
+            trigger.NotifyEnemiesSpawned();
+        }
     }
 }
